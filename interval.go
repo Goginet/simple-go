7196e0e7ba1d8994686afe169382457e7f50def2
Georgy Schapchits <georgy.schapchits@synesis.ru>
2016-09-09 14:27:32 +0300

    add Interval module

---
+++ interval.go
@@ -0,0 +1,40 @@
+package simple_go
+
+// Interval - структура для зранения промежутков времени
+type Interval struct {
+  StartTime, EndTime float64
+}
+
+// Start - установить маркер начала на текущее время
+func (interval *Interval)Start() {
+  interval.StartTime = NowTime()
+  return
+}
+
+// End - установить маркер конца на текущее время
+func (interval *Interval)End() {
+  interval.EndTime = NowTime()
+  return
+}
+
+// GetStart - возвращает время начала отсчета
+func (interval Interval)GetStart() float64 {
+  return interval.StartTime
+}
+
+// GetEnd - возвращает время окончания отсчета
+func (interval Interval)GetEnd() float64 {
+  return interval.EndTime
+}
+
+// Len - возвращает длинну промежутка
+func (interval Interval)Len() float64 {
+  if interval.EndTime == 0 {
+    if interval.StartTime == 0 {
+      return 0
+    }
+    return float64(NowTime() - interval.StartTime)
+  } else {
+    return float64(interval.EndTime - interval.StartTime)
+  }
+}
